99bf888ed2871e76864fc1ad88a289028a6e89c3
ucheckmyemail <ucheckmyemail@hotmail.com>
2018-03-02 17:55:13 -0700

    First commit

---
+++ OS/HW2/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/marinaj/Documents/OS/HW2/hello.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.13.0-141-generic/scripts/module-common.lds --build-id  -o /home/marinaj/Documents/OS/HW2/hello.ko /home/marinaj/Documents/OS/HW2/hello.o /home/marinaj/Documents/OS/HW2/hello.mod.o
---
+++ OS/HW2/.tmp_versions/my-module.mod
@@ -0,0 +1,2 @@
+/home/marinaj/Documents/OS/HW2/my-module.ko
+/home/marinaj/Documents/OS/HW2/my-module.o
